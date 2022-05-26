594117416e1fa7b55920be64419c58039af105ac
vivxk <viveksankhla222@gmail.com>
2022-05-26 11:02:47 +0530

    Update CenterService and its implementation

--- src/main/java/com/backend/liv2train/repositories/CenterRepo.java
+++ src/main/java/com/backend/liv2train/repositories/CenterRepo.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.backend.liv2train.entities.Center;
 
 public interface CenterRepo extends JpaRepository<Center, String> {
-
 }
--- src/main/java/com/backend/liv2train/service/CenterService.java
+++ src/main/java/com/backend/liv2train/service/CenterService.java
@@ -13,4 +13,5 @@ public interface CenterService {
 	Center getCenterByCode(String centercode);
 
 //	Center getCenterByName(String centerName);
+
 }
